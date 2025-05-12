3ca5eb7c19944269f093eb2b0c97251b0e54c15a
connorrees345 <rees_connor@hotmail.co.uk>
2025-05-12 23:09:22 +0100

    Update Joke.js

--- models/Joke.js
+++ models/Joke.js
@@ -1,1 +1,9 @@
+const mongoose = require('mongoose');
 
+const jokeSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  joke: { type: String, required: true },
+  status: { type: String, default: 'pending' }
+});
+
+module.exports = mongoose.model('Joke', jokeSchema);
